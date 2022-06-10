dd0c2da91549ad2703461138a03a9e61b98312b8
stefanlevianto <stefanlevianto@gmail.com>
2022-06-10 09:36:51 +0700

    commit 1

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const fs = require("fs/promises");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let totalEmosi = null;
+    const arrGabunganReview = (await promiseTheaterIXX()).concat(
+      await promiseTheaterVGC()
+    );
+    for (let i = 0; i < arrGabunganReview.length; i++) {
+      if (arrGabunganReview[i].hasil === emosi) {
+        totalEmosi = totalEmosi + 1;
+      }
+    }
+    return totalEmosi;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
